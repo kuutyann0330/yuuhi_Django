c53509cf99d2eb31808e4498018a6e06254da3e3
kuutyann0330 <kuutyann0330@gmail.com>
2025-11-19 13:33:41 +0900

    urls.py を更新

--- contact/urls.py
+++ contact/urls.py
@@ -1,2 +1,10 @@
 from django.urls import path
 from . import views
+
+app_name = "inquiry"
+
+urlpatterns = [
+    #  どんなパス, 動かす関数, このパスの名前
+    path('', views.index, name='index'),
+    path('article/list', views.list, name='list'),
+]
